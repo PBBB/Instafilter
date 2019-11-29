922a3af2a895de070aa4dbf061d8ec93504facda
Issac Penn <pbbsignup@outlook.com>
2019-11-29 12:27:41 +0800

    Day 66 done

---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,26 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by PBB on 2019/11/29.
+//  Copyright © 2019 Issac Penn. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
